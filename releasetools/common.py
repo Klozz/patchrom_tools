61db15ac1302f44001ee5b59f566921998f40345
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-07-01 01:00:32 -0500

    Add support to f2fs a bml :)

--- releasetools/common.py
+++ releasetools/common.py
@@ -823,6 +823,7 @@ def ComputeDifferences(diffs):
 # map recovery.fstab's fs_types to mount/format "partition types"
 PARTITION_TYPES = { "yaffs2": "MTD", "mtd": "MTD", "ext3": "EMMC",
                     "ext4": "EMMC", "emmc": "EMMC", "vfat": "EMMC",
+		    "f2fs": "EMMC","bml": "BML",
 					"auto": "EMMC"}
 
 def GetTypeAndDevice(mount_point, info):
